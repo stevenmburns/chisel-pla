f03b83ef25476eaa65b3a94b0ec7e604ba2ee99a
Steven Burns <smburns47@gmail.com>
2022-11-18 15:18:15 -0800

    Add random tester

--- src/main/scala/alu/alu.scala
+++ src/main/scala/alu/alu.scala
@@ -1,5 +1,3 @@
-// See README.md for license details.
-
 package alu
 
 import chisel3._
@@ -11,64 +9,91 @@ class AluIfc extends Module {
   val b = IO(Input(Vec(4, UInt(16.W))))
   val z = IO(Output(Vec(4, UInt(16.W))))
 
-  val opcode = IO(Input(UInt(4.W)))
+  val opcode = IO(Input(UInt(2.W)))
   val mode = IO(Input(UInt(3.W)))
-}
-
-
-class Alu extends AluIfc {
 
   val opcodes = Seq[(Int,(UInt,UInt)=>UInt)](
     (0, {_ + _}),
     (1, {_ - _}),
     (2, {_ & _}),
     (3, {_ | _}))
+}
 
+class Alu extends AluIfc {
   z := DontCare
 
   for { k <- Seq(1, 2, 4) } {
-
     when (mode === k.U) {
       val a1 = a.asTypeOf(Vec(4/k, UInt((16*k).W)))
       val b1 = b.asTypeOf(Vec(4/k, UInt((16*k).W)))
       val z1 = Wire(Vec(4/k, UInt((16*k).W)))
-
       z1 := DontCare
-
       for { (op, f) <- opcodes } {
-        for { ((aa, bb), zz) <- (a1 zip b1) zip z1 } {
-          zz := f(aa, bb)
+        when (opcode === op.U) {
+          for { ((aa, bb), zz) <- (a1 zip b1) zip z1 } {
+            zz := f(aa, bb)
+          }
         }
       }
-
-
-      when (opcode === 0.U) {
-        for { ((aa, bb), zz) <- (a1 zip b1) zip z1 } {
-          zz := aa + bb
-        }
-      } .elsewhen (opcode === 1.U) {
-        for { ((aa, bb), zz) <- (a1 zip b1) zip z1 } {
-          zz := aa - bb
-        }
-      } .elsewhen (opcode === 2.U) {
-        for { ((aa, bb), zz) <- (a1 zip b1) zip z1 } {
-          zz := aa & bb
-        }
-      } .elsewhen (opcode === 3.U) {
-        for { ((aa, bb), zz) <- (a1 zip b1) zip z1 } {
-          zz := aa | bb
-        }
-      }
-
       z := z1.asTypeOf(Vec(4, UInt(16.W)))
-
     }
   }
 
+  printf("mode=%x opcode=%x a=%x b=%x z=%x\n", mode, opcode, a.asUInt, b.asUInt, z.asUInt)
+}
+
+class AluMMX extends AluIfc {
+  {
+    val ci = WireInit(init=false.B)
+    val bn = WireInit(init=b)
+    when (opcode === 1.U) {
+      ci := true.B
+      bn(0) := ~b(0)
+      bn(1) := ~b(1)
+      bn(2) := ~b(2)
+      bn(3) := ~b(3)
+    }
+
+    val sa1 = WireInit(init=ci)
+    val sb1 = WireInit(init=ci)
+    val sa2 = WireInit(init=ci)
+    val sb2 = WireInit(init=ci)
+
+    when (mode === 2.U || mode === 4.U) {
+      sa1 := false.B
+      sb1 := true.B
+    }
+
+    when (mode === 4.U) {
+      sa2 := false.B
+      sb2 := true.B
+    }
+
+    val a1 = a(3)  ## sa1 ## a(2)  ## sa2 ## a(1)  ## sa1 ## a(0)
+    val b1 = bn(3) ## sb1 ## bn(2) ## sb2 ## bn(1) ## sb1 ## bn(0)
+
+    val z1 = WireInit(UInt(67.W),init=DontCare)
+
+    for { (op, f) <- opcodes } {
+      if (op >= 2) {
+        when (opcode === op.U) {
+          z1 := f(a1, b1)
+        }
+      }
+    }
+
+    when (opcode < 2.U) {
+      z1 := a1 + b1 + ci
+    }
+
+    z(0) := z1(15,0)
+    z(1) := z1(32,17)
+    z(2) := z1(49,34)
+    z(3) := z1(66,51)
+
+  }
 
   printf("mode=%x opcode=%x a=%x b=%x z=%x\n", mode, opcode, a.asUInt, b.asUInt, z.asUInt)
-
-
 }
 
 object Main extends App {
--- src/test/scala/alu/ALUSpec.scala
+++ src/test/scala/alu/ALUSpec.scala
@@ -6,11 +6,12 @@ import chisel3._
 import chiseltest._
 import org.scalatest.freespec.AnyFreeSpec
 import chisel3.experimental.BundleLiterals._
+import scala.util.Random
 
-class AluSpec extends AnyFreeSpec with ChiselScalatestTester {
+class AluSpecTester(factory : () => AluIfc) extends AnyFreeSpec with ChiselScalatestTester {
 
   "Alu should work" in {
-    test(new Alu) { dut =>
+    test(factory()) { dut =>
       dut.mode.poke(1)
 
       dut.opcode.poke(0)
@@ -156,3 +157,95 @@ class AluSpec extends AnyFreeSpec with ChiselScalatestTester {
     }
   }
 }
+
+class AluRandomTester(factory : () => AluIfc) extends AnyFreeSpec with ChiselScalatestTester {
+
+  "Alu should work" in {
+    test(factory()) { dut =>
+
+      def alu_aux(opcode: BigInt, a: Seq[BigInt], b: Seq[BigInt], mask: BigInt) : Seq[BigInt] = {
+        val z = for {(aa, bb) <- a zip b} yield {
+          if (opcode == 0) {
+            (aa + bb) & mask
+          } else if (opcode == 1) {
+            (aa - bb) & mask
+          } else if (opcode == 2) {
+            aa & bb
+          } else {
+            aa | bb
+          }
+        }
+        z
+      }
+
+      def alu_gold(mode: BigInt, opcode: BigInt, a: Seq[BigInt], b: Seq[BigInt]): Seq[BigInt] = {
+
+        val m = (1<<16) - 1
+
+        if (mode == 1) {
+          alu_aux( opcode, a, b, (BigInt(1)<<16)-1)
+        } else if ( mode == 2) {
+          val aa = Seq((a(1)<<16) | a(0), (a(3)<<16) | a(2))
+          val bb = Seq((b(1)<<16) | b(0), (b(3)<<16) | b(2))
+          val zz = alu_aux( opcode, aa, bb, (BigInt(1)<<32)-1)
+          Seq(zz(0)&m, (zz(0)>>16)&m, zz(1)&m, (zz(1)>>16)&m)
+        } else {
+          val aa = Seq((a(3)<< 48) | (a(2) << 32) | (a(1)<<16) | a(0))
+          val bb = Seq((b(3)<< 48) | (b(2) << 32) | (b(1)<<16) | b(0))
+          val zz = alu_aux( opcode, aa, bb, (BigInt(1)<<64)-1)
+          Seq(zz(0)&m, (zz(0)>>16)&m, (zz(0)>>32)&m, (zz(0)>>48)&m)
+        }
+      }
+
+
+      def set_and_check(mode: Int, opcode: Int,
+        a : Seq[BigInt], b : Seq[BigInt]) : Unit = {
+
+        val z = alu_gold(mode, opcode, a, b)
+
+        println(s"mode=$mode opcode=$opcode a=$a b=$b expected=$z")
+
+        dut.mode.poke(mode)
+        dut.opcode.poke(opcode)
+        for {i <- 0 until 4} {
+          dut.a(i).poke(a(i))
+          dut.b(i).poke(b(i))
+        }
+        dut.clock.step()
+        for {i <- 0 until 4} {
+          dut.z(i).expect(z(i))
+        }
+      }
+
+
+
+      val m1 = (1<<16)-1
+      set_and_check(1, 0, Seq(0,1,2,3), Seq(3,2,1,0))
+      set_and_check(1, 1, Seq(0,1,2,3), Seq(3,2,1,0))
+      set_and_check(1, 2, Seq(0,1,2,3), Seq(3,2,1,0))
+      set_and_check(1, 3, Seq(0,1,2,3), Seq(3,2,1,0))
+      set_and_check(1, 0, Seq(m1,m1,m1,m1), Seq(1,0,0,0))
+      set_and_check(2, 0, Seq(m1,m1,m1,m1), Seq(1,0,0,0))
+      set_and_check(4, 0, Seq(m1,m1,m1,m1), Seq(1,0,0,0))
+      set_and_check(1, 1, Seq(1,0,0,0), Seq(1,1,1,1))
+      set_and_check(2, 1, Seq(1,0,0,0), Seq(1,0,1,0))
+      set_and_check(4, 1, Seq(1,0,0,0), Seq(1,0,0,0))
+
+      val rnd = new Random()
+
+      for {i <- 0 until 1000} {
+        val mode = Seq(1,2,4)(rnd.nextInt(3))
+        val opcode = Seq(0,1,2,3)(rnd.nextInt(4))
+        val a = for {j <- 0 until 4} yield BigInt(16, rnd)
+        val b = for {j <- 0 until 4} yield BigInt(16, rnd)
+
+        set_and_check(mode, opcode, a, b)
+      }
+    }
+  }
+}
+
+
+//class AluSpecTest extends AluSpecTester(() => new Alu) 
+//class AluMMXSpecTest extends AluSpecTester(() => new AluMMX)
+class AluMMXRandomTest extends AluRandomTester(() => new AluMMX)
