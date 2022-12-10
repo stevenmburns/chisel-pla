88b726dcdd6c65b1080e05abaab0cd2b5e736d4d
Steven Burns <smburns47@gmail.com>
2022-12-09 16:01:06 -0800

    New examples

---
+++ src/main/scala/parallel_prefix/parallel_prefix.scala
@@ -0,0 +1,186 @@
+package parallel_prefix
+
+import chisel3._
+import chisel3.util._
+import chisel3.experimental.BundleLiterals._
+
+object helpers {
+  def largestPow2LessThan(x: BigInt): BigInt = {
+    var y = BigInt(1)
+    while (2 * y < x) {
+      y *= 2
+    }
+    y
+  }
+}
+
+class ParallelPrefix[T <: Data](gen: T, val n: Int) extends Module {
+  val io = IO(new Bundle {
+    val inp = Input(Vec(n, gen.cloneType))
+    val out = Output(Vec(n, gen.cloneType))
+  })
+}
+
+class Serial[T <: Data](gen: T, n: Int, op: (T, T) => T)
+    extends ParallelPrefix(gen, n) {
+  io.out(0) := io.inp(0)
+  var prev = io.out(0)
+  for {i <- 1 until n} {
+    io.out(i) := op(prev, io.inp(i))
+    prev = io.out(i)
+  }
+}
+
+class Sklansky[T <: Data](gen: T, n: Int, op: (T, T) => T)
+    extends ParallelPrefix(gen, n) {
+
+  if (n == 1) {
+    io.out(0) := io.inp(0)
+  } else {
+    val m = helpers.largestPow2LessThan(n).toInt
+
+    val u = Module(new Sklansky(gen, m, op))
+    val v = Module(new Sklansky(gen, n-m, op))
+
+    for {i <- 0 until n} {
+      if (i < m) {
+        u.io.inp(i) := io.inp(i)
+        io.out(i) := u.io.out(i)
+      } else {
+        v.io.inp(i-m) := io.inp(i)
+        io.out(i) := op(io.out(m-1),v.io.out(i-m))
+      }
+    }
+  }
+}
+
+class SklanskyFlat[T <: Data](gen: T, n: Int, op: (T, T) => T)
+    extends ParallelPrefix(gen, n) {
+
+  def aux(inp: Vec[T]): Vec[T] = {
+    val n = inp.length
+    if (n == 1) {
+      inp
+    } else {
+      val m = helpers.largestPow2LessThan(n).toInt
+
+      val inp0 = VecInit(IndexedSeq.tabulate(m){ i => inp(i) })
+      val inp1 = VecInit(IndexedSeq.tabulate(n-m){ i => inp(i+m) })
+
+      val out0 = aux(inp0)
+      val out1 = aux(inp1)
+
+      VecInit(IndexedSeq.tabulate(n){ i =>
+        if (i < m) {
+          out0(i)
+        } else {
+          op(out0(m-1),out1(i-m))
+        }
+      })
+    }
+  }
+
+  io.out := aux(io.inp)
+}
+
+
+class KPG extends Bundle {
+  val notk = Bool()
+  val g = Bool()
+}
+
+object KPG {
+  val k = (new KPG).Lit(_.notk -> false.B, _.g -> false.B)
+  val p = (new KPG).Lit(_.notk -> true.B, _.g -> false.B)
+  val g = (new KPG).Lit(_.notk -> true.B, _.g -> true.B)
+
+  def op(a: KPG, b: KPG): KPG = {
+    Mux(b === p, a, b)
+  }
+}
+
+class PriorityEncoderIfc(val n: Int) extends Module {
+  val io = IO(new Bundle {
+    val a = Input(UInt(n.W))
+    val z = Output(UInt(n.W))
+  })
+}
+
+class PriorityEncoderSimple(n: Int) extends PriorityEncoderIfc(n) {
+  io.z := PriorityEncoderOH(io.a)
+}
+
+class PriorityEncoder(n: Int, factory: () => ParallelPrefix[Bool]) extends PriorityEncoderIfc(n) {
+  val u = Module(factory())
+
+  u.io.inp := VecInit(IndexedSeq.tabulate(n){ i =>
+    io.a(i)
+  })
+
+  io.z := VecInit(IndexedSeq.tabulate(n){ i =>
+    if (i > 0) {
+      u.io.out(i) && !u.io.out(i-1)
+    } else {
+      u.io.out(i)
+    }
+  }).asUInt
+}
+
+
+class AdderIfc(val n: Int) extends Module {
+  val io = IO(new Bundle {
+    val a = Input(UInt(n.W))
+    val b = Input(UInt(n.W))
+    val z = Output(UInt(n.W))
+  })
+}
+
+class AdderSimple(n: Int) extends AdderIfc(n) {
+  io.z := io.a + io.b
+}
+
+class Adder(n: Int, factory: () => ParallelPrefix[KPG]) extends AdderIfc(n) {
+  val u = Module(factory())
+
+  u.io.inp := VecInit(IndexedSeq.tabulate(n){ i =>
+    val x = WireInit(KPG.p)
+    when        (!io.a(i) && !io.b(i)) {
+      x := KPG.k
+    } .elsewhen ( io.a(i) &&  io.b(i)) {
+      x := KPG.g
+    }
+    x
+  })
+
+  io.z := VecInit(IndexedSeq.tabulate(n){ i =>
+    if (i > 0) {
+      (u.io.out(i-1) === KPG.g) ^ (u.io.inp(i) === KPG.p)
+    } else {
+      (u.io.inp(i) === KPG.p)
+    }
+  }).asUInt
+
+}
+
+
+
+object MainSerialOr extends App {
+  println(getVerilogString(
+    new Serial(Bool(), 5, {(x: Bool, y: Bool) => (x | y)})))
+}
+
+object MainSklanskyOr extends App {
+  println(getVerilogString(
+    new SklanskyFlat(Bool(), 5, {(x: Bool, y: Bool) => (x | y)})))
+}
+
+object MainSklanskyKPG extends App {
+  println(getVerilogString(
+    new SklanskyFlat(new KPG(), 5, KPG.op(_, _))))
+}
+
+object MainAdder extends App {
+  println(getVerilogString(
+    new Adder(5, () => new SklanskyFlat(new KPG(), 5, KPG.op(_, _)))))
+}
+
---
+++ src/test/scala/parallel_prefix/parallel_prefix.scala
@@ -0,0 +1,129 @@
+package parallel_prefix
+
+import chisel3._
+import chiseltest._
+import chiseltest.formal._
+import org.scalatest.freespec.AnyFreeSpec
+import testutil._
+
+import chisel3.experimental.BundleLiterals._
+
+class ParallelPrefixMiter[T <: Data](gen: T, n: Int,
+                                     factory0: () => ParallelPrefix[T],
+                                     factory1: () => ParallelPrefix[T]) extends
+    ParallelPrefix(gen, n) {
+
+  val u0 = Module(factory0())
+  val u1 = Module(factory1())
+  u0.io.inp := io.inp
+  u1.io.inp := io.inp
+  io.out := u0.io.out
+
+  val same = u0.io.out === u1.io.out
+
+  assert(same)
+
+}
+
+class AdderMiter(n: Int, factory0: () => AdderIfc, factory1: () => AdderIfc) extends AdderIfc(n) {
+  val u0 = Module(factory0())
+  val u1 = Module(factory1())
+  u0.io.a := io.a
+  u1.io.a := io.a
+  u0.io.b := io.b
+  u1.io.b := io.b
+
+  io.z := u0.io.z
+
+  val same = u0.io.z === u1.io.z
+
+  assert(same)
+}
+
+class PriorityEncoderMiter(n: Int, factory0: () => PriorityEncoderIfc, factory1: () => PriorityEncoderIfc) extends PriorityEncoderIfc(n) {
+  val u0 = Module(factory0())
+  val u1 = Module(factory1())
+  u0.io.a := io.a
+  u1.io.a := io.a
+  io.z := u0.io.z
+
+  val same = u0.io.z === u1.io.z
+  assert(same)
+}
+
+class AdderSpecTester(tag: String, factory : () => Adder) extends AnyFreeSpec with ChiselScalatestTester with TestParams {
+
+  s"$tag should pass AdderSpec tests" in {
+    test(factory()).withAnnotations(annons) { dut =>
+
+      val rnd = new scala.util.Random()
+
+      for {_ <- 0 until 1000} {
+        val a = BigInt(dut.n, rnd)
+        val b = BigInt(dut.n, rnd)
+        val z = (a + b) & ((BigInt(1)<<dut.n) - 1)
+
+        dut.io.a.poke(a.U)
+        dut.io.b.poke(b.U)
+        dut.clock.step()
+        dut.io.z.expect(z.U)
+      }
+    }
+  }
+}
+
+class AdderSpecTest extends AdderSpecTester("Adder5", () => new Adder(128, () => new SklanskyFlat(new KPG(), 128, KPG.op(_, _))))
+
+
+
+
+
+class ParallelPrefixOrChecker extends AnyFreeSpec with ChiselScalatestTester with Formal {
+  val DefaultAnnos = Seq(BoundedCheck(1), Z3EngineAnnotation)
+  val tag = "SerialOr and SklanskyFlatOr"
+  val gen = Bool()
+  val n = 5
+  val op = {(x: Bool, y: Bool) => (x | y)}
+  val factory0 = () => new Serial(gen, n, op)
+  val factory1 = () => new SklanskyFlat(gen, n, op)
+  s"$tag should be formally equivalent" in {
+    verify(new ParallelPrefixMiter(gen, n, factory0, factory1), DefaultAnnos)
+  }
+}
+
+class ParallelPrefixKPGChecker extends AnyFreeSpec with ChiselScalatestTester with Formal {
+  val DefaultAnnos = Seq(BoundedCheck(1), Z3EngineAnnotation)
+  val tag = "SerialKPG and SklanskyFlatKPG"
+  val gen = new KPG()
+  val n = 64
+  val op = KPG.op(_, _)
+  val factory0 = () => new Serial(gen, n, op)
+  val factory1 = () => new SklanskyFlat(gen, n, op)
+  s"$tag should be formally equivalent" in {
+    verify(new ParallelPrefixMiter(gen, n, factory0, factory1), DefaultAnnos)
+  }
+}
+
+class PriorityEncoderChecker extends AnyFreeSpec with ChiselScalatestTester with Formal {
+  val DefaultAnnos = Seq(BoundedCheck(1), Z3EngineAnnotation)
+  val tag = "SerialPriorityEncoder and SklanskyFlatPriorityEncoder"
+  val n = 5
+  val op = (a: Bool, b: Bool) => a | b
+  val factory0 = () => new PriorityEncoderSimple(n)
+  val factory1 = () => new PriorityEncoder(5, () => new SklanskyFlat(Bool(), 5, op))
+  s"$tag should be formally equivalent" in {
+    verify(new PriorityEncoderMiter(n, factory0, factory1), DefaultAnnos)
+  }
+}
+
+
+class AdderChecker extends AnyFreeSpec with ChiselScalatestTester with Formal {
+  val DefaultAnnos = Seq(BoundedCheck(1), Z3EngineAnnotation)
+  val tag = "SerialAdder and SklanskyFlatAdder"
+  val n = 5
+  val factory0 = () => new AdderSimple(n)
+  val factory1 = () => new Adder(5, () => new SklanskyFlat(new KPG(), 5, KPG.op(_, _)))
+  s"$tag should be formally equivalent" in {
+    verify(new AdderMiter(n, factory0, factory1), DefaultAnnos)
+  }
+}
